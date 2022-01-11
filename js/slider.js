9fe247311ae5175fa0cb910039f38edd2fb1769c
Eduardo Bremen <eduardo.bremen92@gmail.com>
2022-01-11 01:59:23 -0600

    carrousel banner 10-01-22

--- js/slider.js
+++ js/slider.js
@@ -23,3 +23,57 @@ function changePosition(change){
 
 // ----------------- SLIDER PORTFOLIO
 
+// let banner = ['img/home/CARRUSEL1.JPG','img/home/CARRUSEL2.JPG','img/home/CARRUSEL3.JPG','img/home/CARRUSEL4.JPG','img/home/CARRUSEL5.JPG','img/home/CARRUSEL6.JPG','img/home/CARRUSEL7.JPG', 'img/home/CARRUSEL8.JPG']
+// const previous = document.getElementById('previous')
+// const nextImg = document.getElementById('next_img')
+// let conteo = 0;
+// let img = document.getElementById('imagen_banner')
+
+// previous.addEventListener('click', ()=> {
+//         if (conteo > 0) {
+//             img.src = banner[conteo - 1];
+//             conteo--;
+//         } else {
+//             img.src = banner[banner.length - 1];
+//             conteo = banner.length - 1;
+//         }
+//     })
+
+// nextImg.addEventListener('click', ()=>{
+//     if(conteo === 0 || conteo < 7){
+//         img.src = banner[conteo + 1]
+//         conteo ++;
+//     } else if (conteo === 7){
+//         img.src = banner[0]
+//         conteo = 0
+//     }
+// })
+
+let banner = ['img/home/CARRUSEL1.JPG','img/home/CARRUSEL2.JPG','img/home/CARRUSEL3.JPG','img/home/CARRUSEL4.JPG','img/home/CARRUSEL5.JPG','img/home/CARRUSEL6.JPG','img/home/CARRUSEL7.JPG', 'img/home/CARRUSEL8.JPG']
+const previous = document.getElementById('previous')
+const nextImg = document.getElementById('next_img')
+let conteo = 0;
+let img = document.getElementById('banner_inicio')
+
+previous.addEventListener('click', ()=> {
+        if (conteo > 0) {
+            img.style.background = `url(${banner[conteo - 1]})`;
+            img.style.backgroundPosition = `center`;
+            conteo--;
+        } else {
+            img.src = banner[banner.length - 1];
+            conteo = banner.length - 1;
+        }
+    })
+
+nextImg.addEventListener('click', ()=>{
+    if(conteo === 0 || conteo < 7){
+        img.style.background = `url(${banner[conteo + 1]})`;
+        img.style.backgroundPosition = `center`;
+        conteo ++;
+    } else if (conteo === 7){
+        img.src = banner[0]
+        conteo = 0
+    }
+})
+
