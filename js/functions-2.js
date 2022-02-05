066edf8ca3a7ccd9b289d5c77fd5d4f482022160
Pablo Barrios <barrios.p@outlook.com>
2022-02-05 16:53:59 -0300

    agregue navecacion entre los portfolios

---
+++ js/functions-2.js
@@ -0,0 +1,23 @@
+
+    // ---------REDIRECCION EN PORTFOLIO.HTML
+    $('#btn_estelar').click (() => {
+        location.href = "./portfolio/port_estelar.html"
+    })
+    $('#btn_gime').click (() => {
+        location.href = "./portfolio/port_gime.html"
+    })
+    $('#btn_oso_esther').click (() => {
+        location.href = "./portfolio/port_osoest.html"
+    })
+    $('#btn_wally').click (() => {
+        location.href = "./portfolio/port_wally.html"
+    })
+    $('#btn_pablo_mire').click (() => {
+        location.href = "./portfolio/port_pablo.html"
+    })
+    $('#btn_wedding').click (() => {
+        location.href = "./portfolio/port_wedding.html"
+    })
+    $('#btn_wicho').click (() => {
+        location.href = "./portfolio/port_wicho.html"
+    })
---
+++ js/functions.js
@@ -0,0 +1,30 @@
+$('document').ready(() => {
+
+
+
+
+    // ---------REDIRECCION EN /PORTFOLIO/
+    $('#estelar').click (() => {
+        location.href = "port_estelar.html"
+    })
+    $('#gime').click (() => {
+        location.href = "port_gime.html"
+    })
+    $('#oso_esther').click (() => {
+        location.href = "port_osoest.html"
+    })
+    $('#wally').click (() => {
+        location.href = "port_wally.html"
+    })
+    $('#pablo_mire').click (() => {
+        location.href = "port_pablo.html"
+    })
+    $('#wedding').click (() => {
+        location.href = "port_wedding.html"
+    })
+    $('#wicho').click (() => {
+        location.href = "port_wicho.html"
+    })
+
+
+})
